cf5f47372c524af3a2e3c8b6fa8c87ecddcf1353
Danny <dannyapaza40@gmail.com>
2025-10-18 01:00:02 -0500

    rutas-implementadas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { TableroComponent } from './componentes/tablero/tablero.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { EditarClienteComponent } from './componentes/editar-cliente/editar-cliente.component';
+import { NoEncontradoComponent } from './componentes/no-encontrado/no-encontrado.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', component: TableroComponent},
+    {path:'login', component:LoginComponent},
+    {path:'cliente/editar/:id', component:EditarClienteComponent},
+    {path:'**', component: NoEncontradoComponent}
+    
+];
