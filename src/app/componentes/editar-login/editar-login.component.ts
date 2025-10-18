9e9cf5b81f85ef88f256072c561f5547299c0a59
Danny <dannyapaza40@gmail.com>
2025-10-18 00:38:14 -0500

    creacion-componentes-iniciales

---
+++ src/app/componentes/editar-login/editar-login.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-editar-login',
+  imports: [],
+  templateUrl: './editar-login.component.html',
+  styleUrl: './editar-login.component.css'
+})
+export class EditarLoginComponent {
+
+}
