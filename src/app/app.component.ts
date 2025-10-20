2b471438fb42b66ccab35f831922d6f249c70234
Danny <dannyapaza40@gmail.com>
2025-10-20 11:24:23 -0500

    Barra de Navegacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PiePaginaComponent } from './componentes/pie-pagina/pie-pagina.component';
+import { CabeceroComponent } from './componentes/cabecero/cabecero.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CabeceroComponent, PiePaginaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
