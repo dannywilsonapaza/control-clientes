f1dc865632213a7701f2c15d79525fd0945092d0
Danny <dannyapaza40@gmail.com>
2025-10-22 15:58:32 -0500

    Agregar Cliente v2

--- src/app/componentes/clientes/clientes.component.ts
+++ src/app/componentes/clientes/clientes.component.ts
@@ -3,15 +3,22 @@ import { Cliente } from '../../modelo/cliente.modelo';
 import { ClienteService } from '../../servicios/cliente.service';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-clientes',
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, FormsModule],
   templateUrl: './clientes.component.html',
   styleUrl: './clientes.component.css'
 })
 export class ClientesComponent {
   clientes: Cliente[] | null = null;
+  cliente: Cliente = {
+    nombre: '',
+    apellido: '',
+    email: '',
+    saldo: undefined
+  }
 
   constructor(private clienteService: ClienteService) {
 
@@ -23,18 +30,18 @@ export class ClientesComponent {
     });
   }
 
-  // getSaldoTotal(): number {
-  //   let saldoTotal: number = 0;
-  //   if (this.clientes) {
-  //     this.clientes.forEach(cliente => {
-  //       if(cliente.saldo !== undefined){
-  //         saldoTotal += cliente.saldo;
-  //       }
-  //     });
-  //   }
-  //   return saldoTotal;
-  // }
+
   getSaldoTotal(): number {
     return this.clientes?.reduce((total, cliente) => total + (cliente.saldo ?? 0), 0) ?? 0;
+  }
+
+  agregar(clienteForm: NgForm): void {
+    const {value, valid} = clienteForm;
+    if(valid){
+      //Agregamos la logica para agregar el cliente
+      
+      //Limpiamos el formulario
+      clienteForm.resetForm();
+    }
   }
 }
