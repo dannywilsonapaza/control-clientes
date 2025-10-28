91983c40455643f338fc471c3b846e14ad328316
Danny <dannyapaza40@gmail.com>
2025-10-28 10:39:49 -0500

    Login v1.0

--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  email: string | null = null;
+  password: string | null = null;
 
+  constructor(private router: Router) {
+
+   }
+
+   login(){
+    console.log('Email:', this.email);
+   }
 }
