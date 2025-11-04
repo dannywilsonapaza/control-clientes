088581f4c6833f63ff5c4cbc061b728c0f0f8d5d
Danny <dannyapaza40@gmail.com>
2025-11-04 16:00:50 -0500

    deploy

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,12 @@ import { TableroComponent } from './componentes/tablero/tablero.component';
 import { LoginComponent } from './componentes/login/login.component';
 import { EditarClienteComponent } from './componentes/editar-cliente/editar-cliente.component';
 import { NoEncontradoComponent } from './componentes/no-encontrado/no-encontrado.component';
+import { LoginGuardianService } from './servicios/login-guardian.service';
 
 export const routes: Routes = [
-    {path:'', component: TableroComponent},
+    {path:'', component: TableroComponent, canActivate: [LoginGuardianService]},
     {path:'login', component:LoginComponent},
-    {path:'cliente/editar/:id', component:EditarClienteComponent},
+    {path:'cliente/editar/:id', component:EditarClienteComponent, canActivate: [LoginGuardianService]},
     {path:'**', component: NoEncontradoComponent}
-    
+
 ];
